8a4815d7b62cc939f513b8781d97cf839da51b09
Eugene Kim <eugkim1997@gmail.com>
2020-07-25 16:10:49 -0700

    worked on sessions

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,13 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  if (req.session === undefined) {
+    return models.Sessions.create()
+    .then(something => {
+
+    })
+  }
+  next();
 };
 
 /************************************************************/
